9453e715e006f3ec85928cd2575c811cc5ac940d
Oleg Korol <oleg@olegkorol.de>
2022-10-21 15:09:27 +0200

    add *a bit* of error handling, move env vars

--- notification-service/utils/send-emails.ts
+++ notification-service/utils/send-emails.ts
@@ -1,8 +1,8 @@
 import nodemailer from "nodemailer";
 import { NewJobPosting } from "../types";
 
-const userEmail = process.env.EMAIL || "talentiocodingchallenge@gmail.com";
-const userPassword = process.env.EMAIL_PASSWORD || "tmqfadpysuefaoja";
+const userEmail = process.env.EMAIL || "";
+const userPassword = process.env.EMAIL_PASSWORD || "";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -12,10 +12,22 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+/**
+ * Send emails to subscribers notifying them about a new job posting.
+ *
+ * @export
+ * @param {string[]} emails
+ * @param {NewJobPosting} newJobPosting
+ * @returns {Promise<void>}
+ */
 export async function sendEmails(
   emails: string[],
   newJobPosting: NewJobPosting
-) {
+): Promise<void> {
+  if (emails.length === 0) {
+    console.log("No emails to send");
+    return;
+  }
   try {
     await transporter.sendMail({
       from: userEmail,
@@ -30,8 +42,10 @@ export async function sendEmails(
       `,
     });
     console.log("Email(s) sent!");
+    return;
   } catch (error) {
-    console.log(error);
+    console.log("Email not sent", error);
+    return;
   }
 }
 
